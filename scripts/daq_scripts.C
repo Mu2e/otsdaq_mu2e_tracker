c5324398ddb97d1df6082215155e8fdc80bf3da2
murat <mu2etrk@mu2edaq13.fnal.gov>
2024-07-04 21:43:31 -0500

    add data integrity check

--- scripts/daq_scripts.C
+++ scripts/daq_scripts.C
@@ -4,6 +4,7 @@
 #include "iostream"
 
 #include "TH1.h"
+#include "TStopwatch.h"
 
 #include "dtcInterfaceLib/DTC.h"
 #include "cfoInterfaceLib/CFO.h"
@@ -289,12 +290,229 @@ int dtc_read_subevents(uint64_t FirstTS = 0, int PrintData = 1, int PcieAddr = -
 
   DtcInterface* dtc_i = DtcInterface::Instance(PcieAddr);
   dtc_i->ReadSubevents(list_of_subevents,FirstTS,PrintData,OutputFn);
+  
   return list_of_subevents.size();
 }
 
 
+struct RocData_t {
+  ushort  nb;
+  ushort  header;
+  ushort  n_data_packets;  // n data packets, 16 bytes each
+  ushort  ewt[3];
+  ushort  status;
+  ushort  xxx2;
+  ushort  data; // array, use it just for memory mapping
+};
+  
 //-----------------------------------------------------------------------------
-void dtc_buffer_test_emulated_cfo(int NEvents=3, uint64_t FirstTS=0, int PrintData = 1, const char* OutputFn = nullptr) {
+// returns number of found errors in the payload data
+//-----------------------------------------------------------------------------
+int validate_dtc_block(ulong EwTag, ushort* Data, ulong* Offset, int PrintData) {
+
+  int nhits[64] = {
+    1,   2,  3,  0,  0,  0,  7,  8,
+    9,  10, 11, 12, 13, 14, 15, 16,
+    0,  20, 21, 22, 12, 13, 11, 12,
+    0,   0,  8,  4, 12, 11, 12, 13,
+    16,  6,  3,  1, 12,  0, 16, 17,
+    18, 19, 12,  1, 12, 12, 11, 11,
+     0,  0,  0,  0, 13, 14, 10, 13,
+    11, 14, 14, 15,  8,  9, 10, 32
+  };
+
+//-----------------------------------------------------------------------------
+// calculate offsets just once
+//-----------------------------------------------------------------------------
+  static int   initialized(0);
+  
+  if (initialized == 0) {
+    //    calculate_offsets(nhits,offset);
+    initialized = 1;
+  }
+//-----------------------------------------------------------------------------
+// check consistency of the lengths
+// 1. total number of 2-byte words
+//
+//-----------------------------------------------------------------------------
+  int ewt    = EwTag % 64 ;
+  int nb_dtc = *Data;
+
+  RocData_t* roc = (RocData_t*) (Data+0x18);
+
+  int nb_rocs = 0;
+  for (int i=0; i<6; i++) {
+    int nb   = roc->nb;
+    nb_rocs += nb;
+    roc      = (RocData_t*) ( ((char*) roc) + roc->nb);
+  }
+
+  int nerr     = 0;
+
+  if (nb_dtc != nb_rocs+0x30) {
+    if (PrintData > 0) printf("ERROR: nb_dtc, nb_rocs : 0x%04x 0x%04x\n",nb_dtc,nb_rocs);
+    nerr += 1;
+  }
+//-----------------------------------------------------------------------------
+// event length checks out, check ROC payload
+// check the ROC payload, assume a hit = 2 packets
+//-----------------------------------------------------------------------------
+  roc = (RocData_t*) (Data+0x18);
+  for (int i=0; i<6; i++) {
+    int nb = roc->nb;
+//-----------------------------------------------------------------------------
+// validate ROC header
+//-----------------------------------------------------------------------------
+    // ... TODO
+    ulong ewtag_roc = ulong(roc->ewt[0]) | (ulong(roc->ewt[1]) << 16) | (ulong(roc->ewt[2]) << 32);
+
+    if (ewtag_roc != EwTag) {
+      if (PrintData > 0) printf("ERROR: roc EwTag ewt_roc : %i 0x%08lx 0x%08lx\n",i,EwTag,ewtag_roc);
+      nerr += 1;
+    }
+    
+    if (roc->nb > 0x10) { 
+//-----------------------------------------------------------------------------
+// non-zero payload
+//-----------------------------------------------------------------------------
+      uint*   pattern  = (uint*) &roc->data;
+      if (PrintData > 10) printf("data[0]  = nb = 0x%04x\n",pattern[0]);
+ 
+      int npackets     = roc->n_data_packets;
+      int npackets_exp = nhits[ewt]*2;       // assume two packets per hit (this number is stored somewhere)
+
+      if (npackets != npackets_exp) {
+        if (PrintData > 0) printf("ERROR: EwTag roc npackets npackets_exp: 0x%08lx %i %5i %5i\n",
+                                  EwTag,i,npackets,npackets_exp);
+        nerr += 1;
+      }
+      
+      if (PrintData > 10) {
+        printf("EwTag, ewt, npackets, npackets_exp,  offset: %10lu %3i %2i %2i %10lu\n",
+               EwTag,  ewt, npackets, npackets_exp, *Offset);
+      }
+
+      uint nw      = npackets*4;        // N 4-byte words
+    
+      for (uint i=0; i<nw; i++) {
+        uint exp_pattern = (i+*Offset) & 0xffffffff;
+    
+        if (pattern[i] != exp_pattern) {
+          nerr += 1;
+          if (PrintData > 0) {
+            printf("ERROR: EwTag, ewt i  payload[i]  offset exp_word: %10lu %3i %3i 0x%08x 0x%08lx 0x%08x\n",
+                   EwTag, ewt, i, pattern[i],*Offset,exp_pattern);
+          }
+        }
+      }
+    }
+    roc = (RocData_t*) (((char*) roc) + roc->nb);
+  }
+  
+  *Offset += 2*4*nhits[ewt];
+
+  if (PrintData > 10) printf("EwTag = %10lx, nb_dtc = %i nerr = %i\n",EwTag,nb_dtc,nerr);
+
+  return nerr;
+}
+
+//-----------------------------------------------------------------------------
+// types of errors:
+// 1. wrong event window marker
+// 2. bit errors in the payload words
+// 3. 
+//-----------------------------------------------------------------------------
+int dtc_read_and_validate(uint64_t NEvents, int PrintData = 1, uint64_t FirstTS = 0, int PcieAddr = -1) {
+
+  ulong   n_read_events(0);
+  int     nerrors      (0);
+  bool    match        (false);
+  ulong   offset       (0);
+  
+  std::vector<std::unique_ptr<DTCLib::DTC_SubEvent>> list_of_subevents;
+
+  DtcInterface* dtc_i = DtcInterface::Instance(PcieAddr);
+  DTC*          dtc   = dtc_i->Dtc();
+
+  while(1) {
+//-----------------------------------------------------------------------------
+// read events
+//-----------------------------------------------------------------------------
+    ulong ts = FirstTS+n_read_events;
+    DTC_EventWindowTag expected_tag = DTC_EventWindowTag(ts);
+    list_of_subevents = dtc->GetSubEventData(expected_tag,match);
+    
+    int   ndtcs         = list_of_subevents.size();
+    if (ndtcs == 0) break;
+    
+    int   nerr          = 0;
+
+    if (PrintData > 10) printf(" --- ts, ndtcs : %10lu %i\n",ts,ndtcs);
+    
+    for (int idtc=0; idtc<ndtcs; idtc++) {
+      DTC_SubEvent* dtc_block = list_of_subevents[idtc].get();
+      int           nb        = dtc_block->GetSubEventByteCount();
+      ulong         tag       = dtc_block->GetEventWindowTag().GetEventWindowTag(true);
+      ushort*       data      = (ushort*) dtc_block->GetRawBufferPointer();
+
+      if (PrintData > 10) dtc_i->PrintBuffer(data,nb/2);
+
+      ulong exp_tag = expected_tag.GetEventWindowTag(true);
+      
+      if (tag != exp_tag) {
+        if (PrintData > 0) printf("ERROR: mismatched event tag: read: %10li  expected: %10lu\n",tag,exp_tag);
+        nerr += 1;
+      }
+
+      int rc(0);
+      rc = validate_dtc_block(tag,data,&offset,PrintData);
+
+      if ((rc > 0) and (PrintData > 0)) {
+        dtc_i->PrintBuffer(data,nb/2);
+      }
+
+      nerr  += rc;
+
+      if (PrintData > 10) printf("  tag: %li   rc : %i\n",tag,rc);
+      
+      if (PrintData > 0) {
+        printf(">>>>> ---- n_read_events   tag idtc nerr: %10lu %10lu %3i %5i\n",n_read_events,tag,idtc,nerr);
+      }
+    }
+
+    if (PrintData > 10) printf("  number of dtc blocks: ndtcs:  %i nerr = %i\n",ndtcs,nerr);
+    nerrors       += nerr;
+    n_read_events += 1;
+//-----------------------------------------------------------------------------
+// it looks that a delay is needed, start from one millisecond
+//-----------------------------------------------------------------------------
+    // std::this_thread::sleep_for(std::chrono::microseconds(10000));
+  }
+
+  printf("[%s]: n_read_events nerrors :  %10lu %10i\n",__func__,n_read_events,nerrors);
+  return nerrors;
+}
+
+//-----------------------------------------------------------------------------
+void dtc_val_test_emulated_cfo(int NEvents=3, int PrintData = 1, uint64_t FirstTS=0, int PcieAddr=-1) {
+  TStopwatch timer;
+  timer.Start();
+  
+  DtcInterface* dtc_i = DtcInterface::Instance(PcieAddr); // assume already initialized
+  dtc_i->RocPatternConfig();
+                                        // 68x25ns = 1700 ns
+  
+  dtc_i->InitEmulatedCFOReadoutMode(68,NEvents+1,FirstTS);
+
+  dtc_read_and_validate(NEvents,PrintData,FirstTS,PcieAddr);
+
+  timer.Stop();
+  print(" timing: RT=%7.3f s, Cpu=%7.3f s\n",timer.RealTime(),timer.CpuTime());
+  
+}
+
+//-----------------------------------------------------------------------------
+void dtc_buffer_test_emulated_cfo(int NEvents=3, int PrintData = 1, uint64_t FirstTS=0, const char* OutputFn = nullptr) {
   int pcie_addr(-1);                                 // assume initialized
   
   DtcInterface* dtc_i = DtcInterface::Instance(-1);  // assume already initialized
