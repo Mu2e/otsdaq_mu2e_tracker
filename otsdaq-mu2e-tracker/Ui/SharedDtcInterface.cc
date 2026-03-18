ee915c3c4a84b13575661e28d77045c6c88cca46
edcallaghan <ejc3@lbl.gov>
2026-03-17 21:12:46 -0500

    implement thin forwards of most interface calls in at shared level

--- otsdaq-mu2e-tracker/Ui/SharedDtcInterface.cc
+++ otsdaq-mu2e-tracker/Ui/SharedDtcInterface.cc
@@ -5,7 +5,7 @@
 #include "otsdaq-mu2e-tracker/Ui/SharedDtcInterface.hh"
 
 SharedDtcInterface::SharedDtcInterface(DTCLib::DTC* dtc):
-		DtcInterface::DtcInterface(dtc),
+		interface(std::make_unique<trkdaq::DtcInterface>(dtc)),
 		_initialized(false){
 	/**/
 }
@@ -14,9 +14,163 @@ bool SharedDtcInterface::PostInitialize(const trkdaq::DtcInterface::DtcConfigura
 	bool rv = false;
 	std::lock_guard lock(_mutex);
 	if (!_initialized){
-		static_cast<trkdaq::DtcInterface*>(this)->PostInitialize(config);
+		_interface->PostInitialize(config);
 		_initialized = true;
 		rv = true;
 	}
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::ReadSettings(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_ReadSettings(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::PulserOn(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_PulserOn(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::PulserOff(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_PulserOff(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::Rates(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_Rates(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::Read(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_Read(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::ReadDeviceID(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_ReadDeviceID(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::ReadGitCommit(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_ReadGitCommit(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::ReadIlp(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_ReadIlp(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::GetKey (Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_GetKey (args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::ReadSpi(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_ReadSpi(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::MeasureThresholds(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_MeasureThresholds(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::ReadThresholds(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_ReadThresholds(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::SetCalDac(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_SetCalDac(args...);
+	return rv;
+}
+
+template<typename... Args>
+int SharedDtcInterface::SetThresholds(Args... args){
+	std::lock_guard lock(_mutex);
+	int rv = _interface->ControlRoc_SetThresholds(args...);
+	return rv;
+}
+
+template<typename... Args>
+bool SharedDtcInterface::FindThreshold(Args... args){
+	std::lock_guard lock(_mutex);
+	bool rv = _interface->FindThreshold(args...);
+	return rv;
+}
+
+template<typename... Args>
+float SharedDtcInterface::ProgramAndQueryThreshold(Args... args){
+	std::lock_guard lock(_mutex);
+	float rv = _interface->ProgramAndQueryThreshold(args...);
+	return rv;
+}
+
+template<typename... Args>
+std::vector<std::string> SharedDtcInterface::GetRocRegistersNames(Args... args){
+	std::lock_guard lock(_mutex);
+	std::vector<std::string> rv = _interface->GetRocRegistersNames(args...);
+	return rv;
+}
+
+template<typename... Args>
+std::vector<uint32_t> SharedDtcInterface::GetRocRegisters(Args... args){
+	std::lock_guard lock(_mutex);
+	std::vector<uint32_t> rv = _interface->GetRocRegisters(args...);
+	return rv;
+}
+
+template<typename... Args>
+std::vector<float> SharedDtcInterface::GetConvertedRocRegisters(Args... args){
+	std::lock_guard lock(_mutex);
+	std::vector<float> rv = _interface->GetConvertedRocRegisters(args...);
+	return rv;
+}
+
+template<typename... Args>
+std::string SharedDtcInterface::GetRocID(Args... args){
+	std::lock_guard lock(_mutex);
+	std::string rv = _interface->GetRocID(args...);
+	return rv;
+}
+
+template<typename... Args>
+std::string SharedDtcInterface::GetRocDesignInfo(Args... args){
+	std::lock_guard lock(_mutex);
+	std::string rv = _interface->GetRocDesignInfo(args...);
+	return rv;
+}
+
+template<typename... Args>
+std::string SharedDtcInterface::GetRocFwGitCommit(Args... args){
+	std::lock_guard lock(_mutex);
+	std::string rv = _interface->GetRocFwGitCommit(args...);
 	return rv;
 }
--- otsdaq-mu2e-tracker/Ui/SharedDtcInterface.hh
+++ otsdaq-mu2e-tracker/Ui/SharedDtcInterface.hh
@@ -8,13 +8,68 @@
 // otsdaq-mu2e-tracker
 #include "otsdaq-mu2e-tracker/Ui/DtcInterface.hh"
 
-class SharedDtcInterface: protected trkdaq::DtcInterface{
+class SharedDtcInterface{
 	public:
 		SharedDtcInterface(DTCLib::DTC* dtc);
 
 		bool PostInitialize(const trkdaq::DtcInterface::DtcConfiguration_t&);
 
+		template<typename... Args>
+		int ReadSettings(Args... args);
+		template<typename... Args>
+		int PulserOn(Args... args);
+		template<typename... Args>
+		int PulserOff(Args... args);
+		template<typename... Args>
+		int Rates(Args... args);
+		template<typename... Args>
+		int Read(Args... args);
+		template<typename... Args>
+		int ReadDeviceID(Args... args);
+		template<typename... Args>
+		int ReadGitCommit(Args... args);
+		template<typename... Args>
+		int ReadIlp(Args... args);
+		template<typename... Args>
+		int GetKey (Args... args);
+		template<typename... Args>
+		int ReadSpi(Args... args);
+		template<typename... Args>
+		int MeasureThresholds(Args... args);
+		template<typename... Args>
+		int ReadThresholds(Args... args);
+		template<typename... Args>
+		int SetCalDac(Args... args);
+		template<typename... Args>
+		int SetThresholds(Args... args);
+
+		template<typename... Args>
+		int FindAlignment (Args... args);
+		template<typename... Args>
+		int FindAlignments(Args... args);
+
+		template<typename... Args>
+		bool FindThreshold(Args... args);
+
+		template<typename... Args>
+		float ProgramAndQueryThreshold(Args... args);
+
+		template<typename... Args>
+		std::vector<std::string> GetRocRegistersNames(Args... args);
+		template<typename... Args>
+		std::vector<uint32_t> GetRocRegisters(Args... args);
+		template<typename... Args>
+		std::vector<float> GetConvertedRocRegisters(Args... args);
+
+		template<typename... Args>
+		std::string GetRocID(Args... args);
+		template<typename... Args>
+		std::string GetRocDesignInfo(Args... args);
+		template<typename... Args>
+		std::string GetRocFwGitCommit(Args... args);
+
 	protected:
+		std::unique_ptr<trkdaq::DtcInterface> _interface;
 		std::mutex _mutex;
 		bool _initialized;
 
